dd3171ac9c0707d4c2f39a9a8df78c4d2ac38f16
Simikao <s.wierzejski.420@studms.ug.edu.pl>
2024-04-16 21:12:35 +0200

    feat(lab02): Add finding weak number fundations

--- lab02/main.go
+++ lab02/main.go
@@ -63,7 +63,35 @@ func findStrongNumber(nick string) *Factorial {
 
 	return number
 }
+func fibonacci(n int, arr *[]int) int {
+	(*arr)[n] += 1
+	if n <= 1 {
+		return n
+	}
+	return fibonacci(n-1, arr) + fibonacci(n-2, arr)
+}
 
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func findWeakNumber(strongNumber int64, arr *[]int) {
+	intSN := int(strongNumber)
+	difference := abs(intSN - (*arr)[len(*arr)-1])
+	for i := (len(*arr) - 1); i >= 0; i-- {
+		fmt.Println(intSN, " - ", (*arr)[i], " = ", intSN-(*arr)[i])
+		n := (*arr)[i]
+		if abs(intSN-n) < difference {
+			difference = abs(intSN - n)
+		} else {
+
+		}
+	}
+	fmt.Println(len(*arr), intSN)
+}
 func main() {
 	me := Person{"Pioterl", "Arłenski"}
 	nick := string(strings.ToLower(replacer.Replace(me.Name)[0:3]))
@@ -71,4 +99,10 @@ func main() {
 	fmt.Println(nick)
 	result := findStrongNumber(nick)
 	fmt.Printf("For the name %s %s, whose nick is %q, the strong number is %d\n", me.Name, me.LastName, nick, result.iteration)
+
+	arr := make([]int, 31)
+	fibonacci(30, &arr)
+	fmt.Println(arr)
+
+	findWeakNumber(result.iteration, &arr)
 }
